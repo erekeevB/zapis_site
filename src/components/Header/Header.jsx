8dd82003c187353717e75f990f2a62501172bc4b
Batyrkhan <batyrkhan.yerekeyev@nu.edu.kz>
2020-10-31 18:23:17 +0600

    Init state

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+
+    return (
+
+        <div>
+
+            <img />
+            
+
+        </div>
+        
+    )
+
+}
---
+++ src/components/Header/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Navbar = () => {
+
+    return (
+
+        <ul>
+
+            <li>Главная</li>
+            <li>Услуги</li>
+            <li>Наши работники</li>
+            <li>Контакты</li>
+            <li>О нас</li>
+            <li><button>Записаться сейчас</button></li>
+
+        </ul>
+        
+    )
+
+}
+
+export default Navbar
